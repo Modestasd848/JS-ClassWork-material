13c661d89cd05a1ae9acf8f036c2af3df2371632
Modestasd848 <modestasd848@gmail.com>
2023-01-31 13:17:24 +0200

    Task 10 and 9 answers, css functions

--- Task 10/script.js
+++ Task 10/script.js
@@ -2,6 +2,15 @@
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
 
+//import from './modules/math/'
+import { composition } from './modules/math/composition.js';
+import { division } from './modules/math/division.js';
+import { multiplication } from './modules/math/multiplication.js';
+import { substraction } from './modules/math/subtraction.js';
+
+//import numbers from './modules/numbers/numbers.js'
+import { one, two, three, four, five } from './modules/numbers/numbers.js';
+
 let a = composition(one, four);
 let b = division(four, two);
 let c = substraction(three, two);
